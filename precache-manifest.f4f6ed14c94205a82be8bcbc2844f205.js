ce630964bbdb6d43fc8e8984d3579631765475fb
Jullierme Silva Barros <julliermegyn@gmail.com>
2019-12-09 10:03:46 -0300

    Updates

---
+++ precache-manifest.f4f6ed14c94205a82be8bcbc2844f205.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d68db0feb1034af3f8e86e6754bd7628",
+    "url": "/sky-react-challenge/index.html"
+  },
+  {
+    "revision": "6f9bbe487bb9ae981c1d",
+    "url": "/sky-react-challenge/static/js/2.b9420d22.chunk.js"
+  },
+  {
+    "revision": "376e35e6d7056a8ceed9",
+    "url": "/sky-react-challenge/static/js/main.b1e0e882.chunk.js"
+  },
+  {
+    "revision": "9ad8fb2d5267ae0a05ca",
+    "url": "/sky-react-challenge/static/js/runtime-main.d6916820.js"
+  }
+]);
